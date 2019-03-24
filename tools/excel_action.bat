30bf67594020c16d47e1974a0e8893645974fb7d
adaptation <077t6728@gmail.com>
2019-03-25 02:37:13 +0900

    Excel作成スクリプトキックバッチ スクリプトを動的に取得するよう修正

--- tools/excel_action.bat
+++ tools/excel_action.bat
@@ -1,5 +1,5 @@
 @echo off
 echo 休暇届け作成を開始します
-powershell -NoProfile -ExecutionPolicy Unrestricted C:/Program` Files/WORK/chatbot_demo/tools/excel_test.ps1 %1 %2 %3 >> .\excel_action.log 2>&1
+powershell -NoProfile -ExecutionPolicy Unrestricted %~dp0/excel_test.ps1 %1 %2 %3 >> .\excel_action.log 2>&1
 echo 休暇届け作成を終了します。
 exit
